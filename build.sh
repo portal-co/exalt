ad4abe2b7289bce62a61e427202ff22e7bf310d3
Graham Kelly <gkgoat6700@gmail.com>
2025-02-05 01:10:48 +0000

    reorg, rust

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ R=$(dirname $0)
 O=${O:-$R/build/$ARCH}
 mkdir -p $O
 export CFLAGS=${CFLAGS:-}
-$R/compile.sh -isystem $R/include -c -o $O/setjmp_core.o $R/setjmp_core_$ARCH.S
-$R/compile.sh  -isystem $R/include -c -o $O/setjmp.o $R/setjmp.c
+$R/compile.sh -isystem $R/include -c -o $O/setjmp_core.o $R/lib/setjmp_core_$ARCH.S
+$R/compile.sh  -isystem $R/include -c -o $O/setjmp.o $R/lib/setjmp.c
 $R/dlmalloc/build.sh
